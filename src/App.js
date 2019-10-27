b8201962745729abbc73423a342d80aa594042d9
Karen Garcia <kgarcia@i3screen.com>
2019-10-27 15:10:15 -0600

    working on calculation and best way to store information

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const FormDiv = ({title, value, changeParameter, type})=>{
+  return(
+    <form className="inputGroup">
+      <label className="inputBox">
+        {title}
+        <input 
+          type={type}
+          value={value}
+          onChange={e=>changeParameter(e.target.value)}
+        />
+      </label>
+    </form>
+  )
+}
+
+
 function App() {
+
+  const [principal, setPrincipal] = useState(172000);
+  const [interestRate, setInterestRate] = useState(3.75);
+  const [monthlyPayment, setMonthlyPayment] = useState(800);
+  const [extraPayment, setExtraPayment] = useState();
+
+  const [result, setResult] = useState([]);
+ 
+
+  const AmortizationSchedule = () =>{
+    const scheduleArray = [];
+    const initialPrinipal = principal;
+    let newPrincipal = principal;
+    const setMonthly = monthlyPayment;
+    const interestSchedule = (interestRate*.01)/12;
+    let monthDate = 1;
+
+    let paymentInterest = newPrincipal*interestSchedule;
+    let principalPaid = setMonthly-paymentInterest;
+    console.log({principalPaid});
+    let balance = newPrincipal - principalPaid;
+    principalPaid = balance;
+    console.log(paymentInterest);
+    scheduleArray.push(1, monthlyPayment, principalPaid, paymentInterest, balance);
+    // scheduleArray.push("1");
+    setResult(scheduleArray);
+    console.log({result});
+    console.log({scheduleArray});
+  
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Give no other option not to change!
       </header>
+      <main>
+        <FormDiv
+          title="Loan/Mortgage Amount"
+          value={principal}
+          changeParameter={setPrincipal}
+          type="number"
+        />
+        <FormDiv
+          title="Interest Rate"
+          value={interestRate}
+          changeParameter={setInterestRate}
+          type="number"
+        />
+        <FormDiv
+          title="Set Monthly Payment"
+          value={monthlyPayment}
+          changeParameter={setMonthlyPayment}
+          type="number"
+        />
+        <FormDiv
+          title="Extra payment to monthly amount"
+          value={extraPayment}
+          changeParameter={setExtraPayment}
+          type="number"
+        />
+        <button onClick={AmortizationSchedule}>submit</button>
+      </main>
+      <section >
+        <h2>Amortization schedule</h2>
+        <div id="amortizationResults">
+        
+        {result.map((col, index)=>(
+          <div
+            className="list"
+            key={index}
+          >
+            {col}  
+            </div>
+        ))}
+        </div>
+      </section>
+
     </div>
   );
 }
