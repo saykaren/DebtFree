f6c2712c720b13558f00ae94b71256c983268c7d
Karen Garcia <kgarcia@i3screen.com>
2019-12-21 23:10:20 -0700

    Updates

---
+++ precache-manifest.0d6e723685a65b8786058c5087c4e5af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25ddc74075b43c5cee9ee66145920186",
+    "url": "/DebtFree/index.html"
+  },
+  {
+    "revision": "b3e9d4cd1f9b134a3a72",
+    "url": "/DebtFree/static/css/main.1993044a.chunk.css"
+  },
+  {
+    "revision": "69711e7ce3693c5247ef",
+    "url": "/DebtFree/static/js/2.36c2e947.chunk.js"
+  },
+  {
+    "revision": "b3e9d4cd1f9b134a3a72",
+    "url": "/DebtFree/static/js/main.00d5f84f.chunk.js"
+  },
+  {
+    "revision": "8e18c8265d63186c5d7c",
+    "url": "/DebtFree/static/js/runtime-main.543487ee.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/payitoff/precache-manifest.dba96a8cdeca14056ef0e27e0584c5f3.js"
+  "/DebtFree/precache-manifest.0d6e723685a65b8786058c5087c4e5af.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/payitoff/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DebtFree/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
