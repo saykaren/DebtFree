efbc0ba31fd14d3a7554a5a052a735fdf67c209f
Karen Garcia <kgarcia@i3screen.com>
2019-12-21 23:08:41 -0700

    Updates

---
+++ precache-manifest.dba96a8cdeca14056ef0e27e0584c5f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2daaf1bdc3766d9a47d9349617671b64",
+    "url": "/payitoff/index.html"
+  },
+  {
+    "revision": "98f57a3faf797ef868ee",
+    "url": "/payitoff/static/css/main.1993044a.chunk.css"
+  },
+  {
+    "revision": "69711e7ce3693c5247ef",
+    "url": "/payitoff/static/js/2.36c2e947.chunk.js"
+  },
+  {
+    "revision": "98f57a3faf797ef868ee",
+    "url": "/payitoff/static/js/main.29d90018.chunk.js"
+  },
+  {
+    "revision": "653e00d22cabff0c6ccc",
+    "url": "/payitoff/static/js/runtime-main.da42ae10.js"
+  }
+]);
