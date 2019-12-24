4f04550f43c7b9f8b75133e17a6da1cb83306a50
Karen Garcia <kgarcia@i3screen.com>
2019-12-24 13:39:44 -0700

    Trying router with publishing...

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -12,45 +12,46 @@ import MainStateApp from './MainStateApp';
 
 const App = ()=>{
   return(
-    <MainStateApp />
-    // <Router>
-    // <div className="App">
-    //     <header className="App-header">
-    //       <ul>
-    //         <li>
-    //           <Link to="/">Home</Link>
-    //         </li>
-    //         <li>
-    //           <Link to={
-    //             {
-    //               pathname: "/Amortization",
-    //               state: {
+    <>
+    {/* <MainStateApp /> */}
+    <Router>
+    <div className="App">
+        <header className="App-header">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to={
+                {
+                  pathname: "/Amortization",
+                  state: {
                     
-    //                 from:"./MainStateApp.js"
-    //               }
-    //             }
-    //           }>Amortization</Link>
-    //         </li>
-    //         <li>
-    //           <Link to="/Extra_Payments">Extra Payments</Link>
-    //         </li>
-    //         <li>
-    //           <Link to="/Graphs/Welcome/User">Graphs!</Link>
-    //         </li>
-    //       </ul>
-    //     </header>
-    //       <section id="main">
-    //         <Switch>
-    //           <Route exact path="/" component={Home}/>
-    //           <Route exact path="/Amortization" component={Amortization} />
-    //           <Route exact path="/Extra_Payments" component={ExtraPayments}/>
-    //           <Route exact path="/Graphs/:firstname/:lastname" component={Graphs} />
-    //         </Switch>
-    //       </section>
-    //     <Footer />
-    //   </div>
-    // </Router>
-
+                    from:"./MainStateApp.js"
+                  }
+                }
+              }>Amortization</Link>
+            </li>
+            <li>
+              <Link to="/Extra_Payments">Extra Payments</Link>
+            </li>
+            <li>
+              <Link to="/Graphs/Welcome/User">Graphs!</Link>
+            </li>
+          </ul>
+        </header>
+          <section id="main">
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/Amortization" component={Amortization} />
+              <Route exact path="/Extra_Payments" component={ExtraPayments}/>
+              <Route exact path="/Graphs/:firstname/:lastname" component={Graphs} />
+            </Switch>
+          </section>
+        <Footer />
+      </div>
+    </Router>
+              </>
   )
 }
 
